a760ca7bcb4fd597870e94277262c81c56b11fee
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-12-11 23:25:59 +0700

    Adding modified

--- loginVerifications.py
+++ loginVerifications.py
@@ -1,12 +1,3 @@
-def userLoginVerification(): 
-    if loginUser():
-       menu.menuUser() 
-    else:
-        print("Verification Failed")
-
-def adminLoginVerification():
-    if loginAdmin():
-        menuAdmin()
-    else:
-        print("Verification Failed")
- 
+def halo(nama):
+    print(f"halo", nama)
+halo("ratih")
